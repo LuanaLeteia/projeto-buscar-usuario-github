b7cfa049eaa07ddc4bc9858ff8294d412c2961d7
Luana Letéia <luanaleteia@gmail.com>
2025-12-11 08:02:23 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,19 +4,50 @@ document.getElementById('btn-search').addEventListener('click', () => {
     getUserProfile(userName)
 })
 
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+
+    if (e.key === 'Enter') {
+        getUserProfile(userName)
+    }
+})
+
 async function user(userName) {
     const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json()
 }
 
+async function repos(userName) {
+    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
+    return await response.json()
+}
+
 function getUserProfile(userName) {
     user(userName).then(userData => {
-        let userInfo = `<img src="${userData.avatar_url}" alt="Foto do Perfil"/>
-                        <div class="data">
-                            <h1>${userData.name ?? 'Não possui nome cadastrado 😒'}</h1>
-                            <p>${userData.bio ?? 'Não possui bio cadastrada 🙄'}</p>
+        let userInfo = `<div class="info">
+                            <img src="${userData.avatar_url}" alt="Foto do Perfil"/>
+                            <div class="data">
+                               <h1>${userData.name ?? 'Não possui nome cadastrado 😒'}</h1>
+                               <p>${userData.bio ?? 'Não possui bio cadastrada 🙄'}</p>
+                            </div>
                         </div>`
         
         document.querySelector('.profile-data').innerHTML = userInfo
+
+        getUserRepositories(userName)
+    })
+}
+
+function getUserRepositories(userName){
+    repos(userName).then(reposData => {
+        let repositoriesItens = ""
+        reposData.forEach(repo => {
+            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
+        })
+
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+                                                                 <h2>Repositórios</h2>
+                                                                 <ul>${repositoriesItens}</ul>
+                                                               </div>`
     })
 }
